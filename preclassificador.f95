d988a66432d584197d91e16f3e78a8324c9cfa8e
VictorCarreira <carreiravr@gmail.com>
2017-11-21 10:08:34 -0200

    Adicionando a versão mais recente

--- preclassificador.f95
+++ preclassificador.f95
@@ -13,10 +13,12 @@ PROGRAM preclassificador
 !!!!!!!!!!!!!!!!!!!!!! DECLARAÇÃO DAS VARIÁVEIS GLOBAIS !!!!!!!!!!!!!!!!!!!!!!!!!
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 	
-!INTEGER, PARAMETER::SP = SELECTED_INT_KIND(r=4)
-REAL, PARAMETER::DP = SELECTED_REAL_KIND(p=8, r=8)   
-REAL(KIND=DP):: a1, a2, a3, a4 
-!INTEGER:: i, j, k
+INTEGER, PARAMETER::SP = SELECTED_INT_KIND(r=8)
+REAL, PARAMETER::DP = SELECTED_REAL_KIND(p=12, r=12, RADIX=12)
+INTEGER(KIND=SP):: i, j, k, ij, nt, i1, ic1, ic2, i25   
+REAL(KIND=SP):: a1, a2, a3, a4, a5, a6, branco 
+REAL(KIND=SP), ALLOCATABLE, DIMENSION(:,:)::tr, cl, prof
+REAL(KIND=SP), ALLOCATABLE, DIMENSION(:,:,:)::hip
 CHARACTER(LEN=15):: cab(7), lito(7)
 
 TYPE litologia
@@ -36,8 +38,8 @@ TYPE(propriedade):: densidade, gama, resistividade, velocidade
 !!!!!!!!!!!!!!!!!CRIANDO OS ARQUIVOS E OS FORMATOS  !!!!!!!!!!!!!!!!!!!!!!!!!!!!
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-open(1,file='dados_sint_t1.txt') ! entrada do programa 						
-open(2,file='saida_class.txt')   ! arquivo de saída do programa
+OPEN(1,file='dados_sint_t1.txt') ! entrada do programa 						
+OPEN(2,file='saida_class.txt')   ! arquivo de saída do programa
 OPEN(3,file='dados_sint_c1.txt') ! segunda entrada 		
 OPEN(4,file='folhelho.txt')      ! subset de dados 
 		
@@ -60,85 +62,83 @@ OPEN(4,file='folhelho.txt')      ! subset de dados
 
 
 !Leitura do arquivo de entrada
-	read(1,15) cab    ! cabeçalho
+ READ(1,15) cab    ! cabeçalho
 !	write(6,15) cab
-	read(1,15) cab    ! linha em branco abaixo do cabeçalho
+ READ(1,15) cab    ! linha em branco abaixo do cabeçalho
 !	write(6,15) cab
 
-	ij=1
-	do while (.true.)
-	read(1,*,end=6) branco,a1,a2,a3,a4,a5,a6
-	ij=ij+1
-	end do
-6 continue
-	close(1)
+ ij=1
+ 
+ DO WHILE(.TRUE.)
+  READ(1,*,END=6) branco,a1,a2,a3,a4,a5,a6
+  ij=ij+1
+ END DO
+ 6 CONTINUE
+  
+CLOSE(1)
 
-	nt=ij-1
-	write(6,*) "n de dados de treinamento",nt
+ nt=ij-1
+ WRITE(6,*) "n de dados de treinamento",nt
 
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 
 
-	allocate (tr(1:nt,4),cl(1:nt),prof(1:nt),hip(1:nt,5,9))
+ ALLOCATE(tr(1:nt,4),cl(1:nt),prof(1:nt),hip(1:nt,5,9))
 
-	open(1,file='dados_sint_T1.txt')
+ OPEN(1,file='dados_sint_T1.txt')
 
-	read(1,15) cab    ! cabe�alho
+ READ(1,15) cab    ! cabeçalho
 !	write(6,15) cab
-	read(1,15) cab    ! linha em branco abaixo do cabe�alho
+ READ(1,15) branco    ! linha em branco abaixo do cabeçalho
 !	write(6,15) cab
 		
-	do i=1,nt
-	read(1,*) branco,cl(i),prof(i),tr(i,1),tr(i,2),tr(i,3),tr(i,4)
-	end do
-	close(1)
+ DO i=1,nt
+  READ(1,*) branco,cl(i),prof(i),tr(i,1),tr(i,2),tr(i,3),tr(i,4)
+ END DO 
+ 
+ CLOSE(1)
+
+ DO i=1,nt
+  DO j=1,5
+   IF(cl(i) == dfloat(j))THEN
+     ic1(j)=ic1(j)+1
+	 hip(ic1(j),1,j)=prof(i)
+	 hip(ic1(j),2,j)=tr(ic1(j),1)
+	 hip(ic1(j),3,j)=tr(ic1(j),2)
+	 hip(ic1(j),4,j)=tr(ic1(j),3)
+	 hip(ic1(j),5,j)=tr(ic1(j),4)
+   END IF
+  END DO  
+  i1=5
+  DO j=451,454
+   i1=i1+1
+    IF(cl(i) == dfloat(j))THEN
+	 ic2(j)=ic2(j)+1
+	 hip(ic2(j),1,i1)=prof(i)
+	 hip(ic2(j),2,i1)=tr(ic2(j),1)
+	 hip(ic2(j),3,i1)=tr(ic2(j),2)
+	 hip(ic2(j),4,i1)=tr(ic2(j),3)
+	 hip(ic2(j),5,i1)=tr(ic2(j),4)
+    END IF
+  END DO  
+ END DO
 
 
-	do i=1,nt
-	
-	do j=1,5
-	if(cl(i) == dfloat(j))then
-	ic1(j)=ic1(j)+1
-	hip(ic1(j),1,j)=prof(i)
-	hip(ic1(j),2,j)=tr(ic1(j),1)
-	hip(ic1(j),3,j)=tr(ic1(j),2)
-	hip(ic1(j),4,j)=tr(ic1(j),3)
-	hip(ic1(j),5,j)=tr(ic1(j),4)
-	end if
-	end do 
+ WRITE(6,*) 'n de folhelhos=',ic1(1)
+ WRITE(6,*) 'n de dolomitas=',ic1(2)
+
+ i25=ic1(1)
+ DO i=1, i25
+  WRITE(6,*) 'densidade dos folhelhos=',hip(i,2,1)
+ END DO
 
 
-	i1=5
-	do j=451,454
-	i1=i1+1
-	if(cl(i) == dfloat(j))then
-	ic2(j)=ic2(j)+1
-	hip(ic2(j),1,i1)=prof(i)
-	hip(ic2(j),2,i1)=tr(ic2(j),1)
-	hip(ic2(j),3,i1)=tr(ic2(j),2)
-	hip(ic2(j),4,i1)=tr(ic2(j),3)
-	hip(ic2(j),5,i1)=tr(ic2(j),4)
-	end if
-	end do 
-
-	end do
-
-
-	write(6,*) 'n de folhelhos=',ic1(1)
-	write(6,*) 'n de dolomitas=',ic1(2)
-
-	i25=ic1(1)
-	do i=1, i25
-	write(6,*) 'densidade dos folhelhos=',hip(i,2,1)
-	end do
-
-
-	i25=ic1(2)
-	do i=1, i25
-	write(6,*) 'densidade das dolomitas=',hip(i,2,2)
-	end do
+ i25=ic1(2)
+ DO i=1, i25
+  WRITE(6,*) 'densidade das dolomitas=',hip(i,2,2)
+ END DO 
 
 
 END PROGRAM preclassificador
