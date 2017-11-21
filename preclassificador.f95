a3b96387fa35ef471079f2fed78cd37bf2c63cc2
VictorCarreira <carreiravr@gmail.com>
2017-11-21 17:50:48 -0200

    Declara vetores índices inteiros e zera variáveis

--- preclassificador.f95
+++ preclassificador.f95
@@ -14,12 +14,17 @@ PROGRAM preclassificador
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 	
 INTEGER, PARAMETER::SP = SELECTED_INT_KIND(r=8)
-REAL, PARAMETER::DP = SELECTED_REAL_KIND(p=12, r=12, RADIX=12)
-INTEGER(KIND=SP):: i, j, k, ij, nt, i1, ic1, ic2, i25   
-REAL(KIND=SP):: a1, a2, a3, a4, a5, a6, branco 
-REAL(KIND=SP), ALLOCATABLE, DIMENSION(:,:)::tr, cl, prof
+REAL, PARAMETER::DP = SELECTED_REAL_KIND(p=12, r=12, radix=12)
+
+INTEGER(KIND=SP):: i, j, ij, nt, i1, i25
+INTEGER(KIND=SP), ALLOCATABLE, DIMENSION(:):: ic1, ic2 
+
+REAL(KIND=SP):: a1, a2, a3, a4, a5, a6, branco
+REAL(KIND=SP), ALLOCATABLE, DIMENSION(:):: prof, cl
+REAL(KIND=SP), ALLOCATABLE, DIMENSION(:,:)::tr
 REAL(KIND=SP), ALLOCATABLE, DIMENSION(:,:,:)::hip
-CHARACTER(LEN=15):: cab(7), lito(7)
+
+CHARACTER(LEN=15):: cab(80), lito(80)
 
 TYPE litologia
  INTEGER:: COMP
@@ -31,17 +36,18 @@ TYPE propriedade
  CHARACTER(LEN= 20) :: PALAVRA
 END TYPE propriedade
 
-TYPE(litologia) :: folhelho, dolomita, diabasio, conglomerado, embasamento 
-TYPE(propriedade):: densidade, gama, resistividade, velocidade  
+!TYPE(litologia) :: folhelho, dolomita, diabasio, conglomerado, embasamento 
+!TYPE(propriedade):: densidade, gama, resistividade, velocidade  
+
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 !!!!!!!!!!!!!!!!!CRIANDO OS ARQUIVOS E OS FORMATOS  !!!!!!!!!!!!!!!!!!!!!!!!!!!!
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 OPEN(1,file='dados_sint_t1.txt') ! entrada do programa 						
-OPEN(2,file='saida_class.txt')   ! arquivo de saída do programa
+!OPEN(2,file='saida_class.txt')   ! arquivo de saída do programa
 OPEN(3,file='dados_sint_c1.txt') ! segunda entrada 		
-OPEN(4,file='folhelho.txt')      ! subset de dados 
+!OPEN(4,file='folhelho.txt')      ! subset de dados 
 		
 
 ! Aqui são os formatos criados para edição dos arquivos de entrada e de saída
@@ -73,8 +79,8 @@ OPEN(4,file='folhelho.txt')      ! subset de dados
   READ(1,*,END=6) branco,a1,a2,a3,a4,a5,a6
   ij=ij+1
  END DO
- 6 CONTINUE
-  
+ 6 CONTINUE  
+
 CLOSE(1)
 
  nt=ij-1
@@ -87,11 +93,11 @@ CLOSE(1)
 
  ALLOCATE(tr(1:nt,4),cl(1:nt),prof(1:nt),hip(1:nt,5,9))
 
- OPEN(1,file='dados_sint_T1.txt')
+ OPEN(1,file='dados_sint_t1.txt')
 
  READ(1,15) cab    ! cabeçalho
 !	write(6,15) cab
- READ(1,15) branco    ! linha em branco abaixo do cabeçalho
+ READ(1,15) cab    ! linha em branco abaixo do cabeçalho
 !	write(6,15) cab
 		
  DO i=1,nt
@@ -100,27 +106,32 @@ CLOSE(1)
  
  CLOSE(1)
 
+!Zerando variáveis
+ hip=0d0
+ ic1=0
+ ic2=0
+
  DO i=1,nt
   DO j=1,5
    IF(cl(i) == dfloat(j))THEN
-     ic1(j)=ic1(j)+1
-	 hip(ic1(j),1,j)=prof(i)
-	 hip(ic1(j),2,j)=tr(ic1(j),1)
-	 hip(ic1(j),3,j)=tr(ic1(j),2)
-	 hip(ic1(j),4,j)=tr(ic1(j),3)
-	 hip(ic1(j),5,j)=tr(ic1(j),4)
+    ic1(j)=ic1(j)+1
+     hip(ic1(j),1,j)=prof(i)
+     hip(ic1(j),2,j)=tr(ic1(j),1)
+     hip(ic1(j),3,j)=tr(ic1(j),2)
+     hip(ic1(j),4,j)=tr(ic1(j),3)
+     hip(ic1(j),5,j)=tr(ic1(j),4)
    END IF
   END DO  
   i1=5
   DO j=451,454
    i1=i1+1
-    IF(cl(i) == dfloat(j))THEN
-	 ic2(j)=ic2(j)+1
-	 hip(ic2(j),1,i1)=prof(i)
-	 hip(ic2(j),2,i1)=tr(ic2(j),1)
-	 hip(ic2(j),3,i1)=tr(ic2(j),2)
-	 hip(ic2(j),4,i1)=tr(ic2(j),3)
-	 hip(ic2(j),5,i1)=tr(ic2(j),4)
+    IF(cl(i) == dfloat(j))THEN 
+     ic2(j)=ic2(j)+1
+     hip(ic2(j),1,i1)=prof(i)
+     hip(ic2(j),2,i1)=tr(ic2(j),1)
+     hip(ic2(j),3,i1)=tr(ic2(j),2)
+     hip(ic2(j),4,i1)=tr(ic2(j),3)
+     hip(ic2(j),5,i1)=tr(ic2(j),4)
     END IF
   END DO  
  END DO
@@ -134,11 +145,9 @@ CLOSE(1)
   WRITE(6,*) 'densidade dos folhelhos=',hip(i,2,1)
  END DO
 
-
  i25=ic1(2)
  DO i=1, i25
   WRITE(6,*) 'densidade das dolomitas=',hip(i,2,2)
  END DO 
-
 
 END PROGRAM preclassificador
