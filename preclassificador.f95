ab4f0ea5a9d588cb0bf7402ec7b883989bc11709
VictorCarreira <carreira@gmail.com>
2017-12-04 19:37:06 -0200

    Adiciona versão funcional do classificador

--- preclassificador.f95
+++ preclassificador.f95
@@ -24,8 +24,9 @@ PROGRAM preclassificador
                   !tr(nt,4) - matriz de densidade                    !
                   !tr(i,2) - matriz de raio-gama                     !
                   !tr(i,3) - matriz de resistividade                 !
-                  !tr(i,4) - matriz de velocidade
-                  !lito1
+                  !tr(i,4) - matriz de velocidade                    !
+                  !lito1(:,:) - matriz de litologias                 !
+                  !lito2(:,:) - matriz do padrão sino                !
                   !hip(nt,5,9) - hipermatriz com todos os dados      !
                   !--------------------------------------------------!
 
@@ -37,12 +38,12 @@ PROGRAM preclassificador
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
   INTEGER, PARAMETER::SP = SELECTED_INT_KIND(r=8)
-  INTEGER, PARAMETER::DP = SELECTED_REAL_KIND(10,100)
+  INTEGER, PARAMETER::DP = SELECTED_REAL_KIND(12,100)
 
-  INTEGER(KIND=SP):: i, j, ij, nt, ntc, nlito
-  INTEGER(KIND=SP), ALLOCATABLE, DIMENSION(:):: ic1, ic2
+  INTEGER(KIND=DP):: i, j, ij, nt, ntc, nlito
+  INTEGER(KIND=DP), ALLOCATABLE, DIMENSION(:):: ic1, ic2
 
-    REAL(KIND=DP):: a1, a2, a3, a4, a5, a6
+    REAL(KIND=DP):: a1, a2, a3, a4, a5, a6, dist!, ndim
 
    REAL(KIND=SP):: inicial, final, custocomputacional
    REAL(KIND=DP), ALLOCATABLE, DIMENSION(:):: prof, cl
@@ -183,185 +184,209 @@ PROGRAM preclassificador
     lito1=0d0
     lito2=0d0
 
+    WRITE(6,*) '========================'
+
     PRINT*, 'ic1=',size(ic1,1) !Tamanho das demais litologias
     PRINT*, 'ic2=',size(ic2,1) !Tamanho da gradação do padrão sino
 
 
+    WRITE(6,*) '========================'
 
+    WRITE(6,*) 'n de folhelhos=',ic1(1)
+    WRITE(6,*) 'n de dolomitas=',ic1(2)
+    WRITE(6,*) 'n de congl-emb1=',ic2(1)
 
- ! 	write(6,*) 'n de folhelhos=',ic1(1)
- ! 	write(6,*) 'n de dolomitas=',ic1(2)
- ! 	write(6,*) 'n de congl-emb1=',ic2(1)
+    WRITE(6,*) '========================'
 
- ! 	write(6,*) '========================'
+    DO i=1,ic1(1)
+     WRITE(6,*) 'densidade dos folhelhos=',hip(i,1,1),hip(i,2,1)
+    END DO
 
- ! 	do i=1,ic1(1)
- ! 	write(6,*) 'densidade dos folhelhos=',hip(i,1,1),hip(i,2,1)
- ! 	end do
+    WRITE(6,*) '========================'
 
- ! 	write(6,*) '========================'
+    DO i=1, ic1(2)
+     WRITE(6,*) 'densidade das dolomitas=',hip(i,1,2),hip(i,2,2)
+    END DO
 
- ! 	do i=1, ic1(2)
- ! 	write(6,*) 'densidade das dolomitas=',hip(i,1,2),hip(i,2,2)
- ! 	end do
+    WRITE(6,*) '========================'
 
- ! 	write(6,*) '========================'
- ! 	do i=1,ic1(3)
- ! 	write(6,*) 'densidade dos diabasio=',hip(i,1,3),hip(i,2,3)
- ! 	end do
+    DO i=1,ic1(3)
+     WRITE(6,*) 'densidade dos diabasio=',hip(i,1,3),hip(i,2,3)
+   END DO
 
+   WRITE(6,*) '========================'
 
- ! 	write(6,*) '========================'
- ! 	do i=1,ic1(4)
- ! 	write(6,*) 'densidade dos conglomerado=',hip(i,1,4),hip(i,2,4)
- ! 	end do
+   DO i=1,ic1(4)
+    WRITE(6,*) 'densidade dos conglomerado=',hip(i,1,4),hip(i,2,4)
+   END DO
 
+   WRITE(6,*) '========================'
 
+    DO i=1, ic2(1)
+     WRITE(6,*) 'densidade dos congl-emb1=',hip(i,1,6),hip(i,2,6)
+    END DO
 
+   WRITE(6,*) '========================'
 
- ! 	write(6,*) '========================'
- ! 	do i=1, ic2(1)
- ! 	write(6,*) 'densidade dos congl-emb1=',hip(i,1,6),hip(i,2,6)
- ! 	end do
+    DO i=1, ic2(2)
+     WRITE(6,*) 'densidade dos congl-emb2=',hip(i,1,7),hip(i,2,7)
+    END DO
 
- ! 	write(6,*) '========================'
- ! 	do i=1, ic2(2)
- ! 	write(6,*) 'densidade dos congl-emb2=',hip(i,1,7),hip(i,2,7)
- ! 	end do
+   WRITE(6,*) '========================'
+
 
 !!!!!!!! ABRINDO O SEGUNDO CONJUNTO DE DADOS
- ! 	open(2,file='dados_sint_c1.txt')
 
- ! 	read(2,15) cab    ! cabeçalho
- ! !	write(6,15) cab
- ! 	read(2,15) cab    ! linha em branco abaixo do cabeçalho
- ! !	write(6,15) cab
+  ! open(2,file='dados_sint_c1.txt')
 
-
- !  do i=1,ntc
- ! 	read(2,*) branco,a1,a2,a3,a4,a5,a6
-
- !  end do
- ! 	close(2)
+  !  read(2,15) cabecalho    ! cabeçalho
+      !write(6,15) cabecalho
+  !  read(2,15) branco   ! linha em branco abaixo do cabeçalho
+      !write(6,15) branco
 
 
- !  do i=1,ic1(1)
- !   do j=1,4
-
- !  lito1(i,j)=hip(i,j+1,1)
- ! end do
- ! end do
-
- !  lito2(1,1)=a3
- !  lito2(1,2)=a4
- !  lito2(1,3)=a5
- !  lito2(1,4)=a6
-
- !  do i=1,4
- !  write(6,*) 'primeiro dado a ser classificado=', lito2(1,i)
- !  end do
+  !  do i=1,ntc
+  !    read(2,*) branco,a1,a2,a3,a4,a5,a6
+  !  end do
+    !CLOSE(2)
 
 
- !  call maha(lito1,ic1(1),lito2,1,4,dist)
- !  write(6,*) '========================'
+   DO i=1,ic1(1)
+    DO j=1,4
+     lito1(i,j)=hip(i,j+1,1)
+    END DO
+   END DO
 
- !  write(6,*) 'dist=',dist
- CALL cpu_time(final)
- custocomputacional=final-inicial
- PRINT*, 'Custo Computacional=',custocomputacional, 'segundos'
- PRINT*,' ************ FIM *************'
+    lito2(1,1)=a3
+    lito2(1,2)=a4
+    lito2(1,3)=a5
+    lito2(1,4)=a6
 
- !CONTAINS
+    DO i=1,4
+      WRITE(6,*) 'primeiro dado a ser classificado=', lito2(1,i)
+    END DO
 
-!  SUBROUTINE maha(g11,np1,g22,np2,ndim,dist)
+
+   CALL maha(lito1,ic1(1),lito2,1,4,dist)
+   WRITE(6,*) '========================'
+
+   WRITE(6,*) 'dist=',dist
+   WRITE(6,*) '========================'
+   CALL cpu_time(final)
+   custocomputacional=final-inicial
+   PRINT*, 'Custo Computacional=',custocomputacional, 'segundos'
+   PRINT*,' ************ FIM *************'
+
+ CONTAINS
+
+  SUBROUTINE maha(g11,np1,g22,np2,ndim,dist)
 
 !  	subrotina que calcula a distância de mahalanobis entre
 !  	dois agrupamentos de elementos com dimensão ndim
 
+   IMPLICIT NONE
+    INTEGER, PARAMETER::SP = SELECTED_INT_KIND(r=8)
+    INTEGER, PARAMETER::DP = SELECTED_REAL_KIND(12,100)
 
-! 	implicit real*8(a-h,o-z)
+    INTEGER(KIND=DP), INTENT(IN):: np1
+    INTEGER(KIND=SP), INTENT(IN):: np2, ndim
+    REAL(KIND=DP),INTENT(OUT):: dist
 
-!  	real*8,intent(in)::g1(np1,ndim),g2(np2,ndim)
-!  	real*8,intent(out)::dist
-!  	integer,intent(in)::np1,np2,ndim
+    INTEGER(KIND=SP):: i,j,k
+    REAL(KIND=DP),ALLOCATABLE,DIMENSION(:)::soma, xm1, xm2
+    REAL(KIND=DP),ALLOCATABLE, DIMENSION(:,:):: g1, g2, g1T, g2T, cov1, cov2, &
+    covag, g11, g22, md, mdT, alfa, d2
+
+    ALLOCATE(soma(ndim),xm1(ndim),xm2(ndim))
+
+    ALLOCATE(g1(np1,ndim),g2(np2,ndim),g1T(ndim,np1),g2T(ndim,np2),&
+    cov1(ndim,ndim),cov2(ndim,ndim),covag(ndim,ndim),md(ndim,1),&
+    mdT(1,ndim),alfa(1,ndim),d2(1,1))
+
+  !implicit real*8(a-h,o-z)
+
+    !real*8,intent(in)::g1(np1,ndim),g2(np2,ndim)
+  !  real*8,intent(out)::dist
+  !  integer,intent(in)::np1,np2,ndim
+  !  integer::k
 
 
-!       real*8 g1(np1,ndim),g2(np2,ndim),g1T(ndim,np1),g2T(ndim,np2),&
-!      	cov1(ndim,ndim),cov2(ndim,ndim),covag(ndim,ndim),soma(ndim),&
-!         xm1(ndim),xm2(ndim),g22(np2,ndim),&
-!        md(ndim,1),mdT(1,ndim),alfa(1,ndim),d2(1,1),g11(np1,ndim)
+  !  real*8 g1(np1,ndim),g2(np2,ndim),g1T(ndim,np1),g2T(ndim,np2),&
+  !  cov1(ndim,ndim),cov2(ndim,ndim),covag(ndim,ndim),soma(ndim),&
+  !  xm1(ndim),xm2(ndim),g22(np2,ndim),&
+  !  md(ndim,1),mdT(1,ndim),alfa(1,ndim),d2(1,1),g11(np1,ndim)
 
 
-! 	g1=g11
-! 	g2=g22
+    g1=g11
+    g2=g22
 
 !  	grupo 1
 
-! 	do j=1,ndim
-! 	soma(j)=0d0
-! 	do i=1,np1
-! 	soma(j)=soma(j)+g1(i,j)
-! 	end do
-! 	end do
+  DO j=1,ndim
+    soma(j)=0d0
+    DO i=1,np1
+      soma(j)=soma(j)+g1(i,j)
+    END DO
+  END DO
 
-! 	do i=1,ndim
-! 	xm1(i)=soma(i)/dfloat(np1)
-! 	end do
+  DO i=1,ndim
+    xm1(i)=soma(i)/dfloat(np1)
+  END DO
 
 !  	grupo 2
 
-! 	do j=1,ndim
-! 	soma(j)=0d0
-! 	do i=1,np2
-! 	soma(j)=soma(j)+g2(i,j)
-! 	end do
-! 	end do
+  DO j=1,ndim
+    soma(j)=0d0
+    DO i=1,np2
+      soma(j)=soma(j)+g2(i,j)
+    END DO
+  END DO
 
-
-! 	do i=1,ndim
-! 	xm2(i)=soma(i)/dfloat(np2)
-! 	end do
+  DO i=1,ndim
+    xm2(i)=soma(i)/dfloat(np2)
+  END DO
 
 !  	vetor das diferenças - será escrito sobre a matrizes g1 e g2
 
-! 	do j=1,ndim
-! 	do i=1,np1
-! 	g1(i,j)=g1(i,j)-xm1(j)
-! 	end do
-! 	end do
 
-! 	do j=1,ndim
-! 	do i=1,np2
-! 	g2(i,j)=g2(i,j)-xm2(j)
-! 	end do
-! 	end do
+  DO j=1,ndim
+    DO i=1,np1
+      g1(i,j)=g1(i,j)-xm1(j)
+    END DO
+  END DO
+
+  DO  j=1,ndim
+    DO i=1,np2
+      g2(i,j)=g2(i,j)-xm2(j)
+    END DO
+  END DO
 
 !      --------GRUPO 1 ---------------------
 !  	criando a matriz transposta g1T
 !  	-------------- -------------------
-! 	do i=1,np1    !107 ! número de equações
-! 	do j=1,ndim   !2
-! 	g1T(j,i)=g1(i,j)
-! 	end do
-! 	end do
+  DO i=1,np1    !107 ! número de equações
+    DO j=1,ndim   !2
+      g1T(j,i)=g1(i,j)
+    END DO
+  END DO
 !  ----------------------------------------------------
 !  	 - multiplicação de matrizes
 !  	   multiplicação de g1T por g1
 
-! 	do k=1,ndim
-! 	do j=1,ndim
-! 	cov1(j,k)=0.d0
-! 	do i=1,np1
-! 	cov1(j,k)=cov1(j,k)+g1T(j,i)*g1(i,k)
-! 	end do
-! 	end do
-! 	end do
+  DO k=1,ndim
+    DO j=1,ndim
+      cov1(j,k)=0.d0
+      DO i=1,np1
+        cov1(j,k)=cov1(j,k)+g1T(j,i)*g1(i,k)
+      END DO
+    END DO
+  END DO
 
-! 	do i=1,ndim
-! 	do j=1,ndim
-! 	cov1(i,j)=cov1(i,j)/dfloat(np1)
-! 	end do
-! 	end do
+  DO i=1,ndim
+    DO j=1,ndim
+      cov1(i,j)=cov1(i,j)/dfloat(np1)
+    END DO
+  END DO
 
 !  	write(6,*) '======covariância 1 ======'
 !  	write(6,*) cov1(1,1),cov1(1,2)
@@ -370,62 +395,62 @@ PROGRAM preclassificador
 !      --------GRUPO 2 ---------------------
 !  	criando a matriz transposta g2T
 
-! 	do i=1,np2
-! 	do j=1,ndim
-! 	g2T(j,i)=g2(i,j)
-! 	end do
-! 	end do
+  DO i=1,np2
+    DO j=1,ndim
+      g2T(j,i)=g2(i,j)
+    END DO
+  END DO
 
 !  ---------------------------------------------------
 !  	 - multiplicação de matrizes
 !  	   multiplicação de g2T por g2
 
-! 	do k=1,ndim
-! 	do j=1,ndim
-! 	cov2(j,k)=0.d0
-! 	do i=1,np2
-! 	cov2(j,k)=cov2(j,k)+g2T(j,i)*g2(i,k)
-! 	end do
-! 	end do
-! 	end do
+   DO k=1,ndim
+     DO j=1,ndim
+       cov2(j,k)=0.d0
+       DO i=1,np2
+         cov2(j,k)=cov2(j,k)+g2T(j,i)*g2(i,k)
+       END DO
+     END DO
+   END DO
 
-! 	do i=1,ndim
-! 	do j=1,ndim
-! 	cov2(i,j)=cov2(i,j)/dfloat(np2)
-! 	end do
-! 	end do
+   DO  i=1,ndim
+     DO j=1,ndim
+       cov2(i,j)=cov2(i,j)/dfloat(np2)
+     END DO
+   END DO
 
-!  	write(6,*) '======covariância 2 ======'
-!  	write(6,*) cov2(1,1),cov2(1,2)
-!  	write(6,*) cov2(2,1),cov2(2,2)
+    WRITE(6,*) '======covariância 2 ======'
+    WRITE(6,*) cov2(1,1),cov2(1,2)
+    WRITE(6,*) cov2(2,1),cov2(2,2)
 
 
 !  	-------- covariância agrupada------
 
-! 	do i=1,ndim
-! 	do j=1,ndim
-! 	covag(i,j)=dfloat(np1)*cov1(i,j)/(dfloat(np1+np2))+ &
-!                   dfloat(np2)*cov2(i,j)/(dfloat(np1+np2))
-! 	end do
-! 	end do
+   DO i=1,ndim
+     DO j=1,ndim
+       covag(i,j)=dfloat(np1)*cov1(i,j)/(dfloat(np1+np2))+ &
+       dfloat(np2)*cov2(i,j)/(dfloat(np1+np2))
+     END DO
+   END DO
 
-!  	write(6,*) '======covariância agrupada ======'
-!  	write(6,*) covag(1,1),covag(1,2)
-!  	write(6,*) covag(2,1),covag(2,2)
+    WRITE(6,*) '======covariância agrupada ======'
+    WRITE(6,*) covag(1,1),covag(1,2)
+    WRITE(6,*) covag(2,1),covag(2,2)
 
 !  	inversao da matriz covag - usando subrotina
 
-! 	call INVERT(covag,ndim)
+   CALL INVERT(covag,ndim)
 
-!  	write(6,*) '====== inv covariância agrupada ======'
-!  	write(6,*) covag(1,1),covag(1,2)
-!  	write(6,*) covag(2,1),covag(2,2)
+    WRITE(6,*) '====== inv covariância agrupada ======'
+    WRITE(6,*) covag(1,1),covag(1,2)
+    WRITE(6,*) covag(2,1),covag(2,2)
 
 !  	diferenicas médias
 
-! 	do i=1,ndim
-! 	md(i,1)=xm1(i)-xm2(i)
-! 	end do
+   DO i=1,ndim
+     md(i,1)=xm1(i)-xm2(i)
+   END DO
 
 !  	write(6,*) '====== diferencias medias ======'
 !  	write(6,*) md(1,1)
@@ -433,63 +458,63 @@ PROGRAM preclassificador
 
 !  	criando a matriz transposta mdT
 !  	---------------------------
-! 	do i=1,ndim
-! 	do j=1,1
-! 	mdT(j,i)=md(i,j)
-! 	end do
-! 	end do
+
+  DO i=1,ndim
+    DO j=1,1
+      mdT(j,i)=md(i,j)
+    END DO
+  END DO
 
 !  ----------------------------------------------------
 !  	multiplicação de mdT por cov^-1
 !  	 - multiplicação de matrizes
 
-! 	do k=1,ndim
-! 	do j=1,1
-! 	alfa(j,k)=0.d0
-! 	do i=1,ndim
-! 	alfa(j,k)=alfa(j,k)+mdT(j,i)*covag(i,k)
-! 	end do
-! 	end do
-! 	end do
+
+  DO k=1,ndim
+    DO j=1,1
+      alfa(j,k)=0.d0
+      DO i=1,ndim
+        alfa(j,k)=alfa(j,k)+mdT(j,i)*covag(i,k)
+      END DO
+    END DO
+  END DO
 
 !  ----------------------------------------------------
 !  	multiplicação de alfa por md
 !  	 - multiplicação de matrizes
 
-! 	do k=1,1
-! 	do j=1,1
-! 	d2(j,k)=0.d0
-! 	do i=1,ndim  !2	!
-! 	d2(j,k)=d2(j,k)+alfa(j,i)*md(i,k)
-! 	end do
-! 	end do
-! 	end do
+  DO k=1,1
+    DO j=1,1
+      d2(j,k)=0.d0
+      DO i=1,ndim  !2	!
+        d2(j,k)=d2(j,k)+alfa(j,i)*md(i,k)
+      END DO
+    END DO
+  END DO
 
-! 	dist=dsqrt(d2(1,1))
+  dist=dsqrt(d2(1,1))
 
-!       return
-!       end
-!  END SUBROUTINE maha
+END SUBROUTINE maha
 !  ! cccccccccccccccccccccccccc
 
 
-!   SUBROUTINE INVERT(A,i)
-!       real*8 A(i,i),B(i)
-!       integer i,im,j,k,l
-!       IM=I-1
-!       DO 5 K=1,I
-!       DO 2 J=1,IM
-!     2 B(J)=A(1,J+1)/A(1,1)
-!       B(I)=1.d0/A(1,1)
-!       DO 4 L=1,IM
-!       DO 3 J=1,IM
-!     3 A(L,J)=A(L+1,J+1)-A(L+1,1)*B(J)
-!     4 A(L,I)=-A(L+1,1)*B(I)
-!       DO 5 J=1,I
-!     5 A(I,J)=B(J)
+   SUBROUTINE INVERT(A,i)
 
-!       RETURN
-!       END
-!   END SUBROUTINE INVERT
+       real*8 A(i,i),B(i)
+       integer i,im,j,k,l
+       IM=I-1
+
+       DO 5 K=1,I
+         DO 2 J=1,IM
+           2 B(J)=A(1,J+1)/A(1,1)
+           B(I)=1.d0/A(1,1)
+           DO 4 L=1,IM
+             DO 3 J=1,IM
+               3 A(L,J)=A(L+1,J+1)-A(L+1,1)*B(J)
+               4 A(L,I)=-A(L+1,1)*B(I)
+               DO 5 J=1,I
+                 5 A(I,J)=B(J)
+
+   END SUBROUTINE INVERT
 
 END PROGRAM preclassificador
